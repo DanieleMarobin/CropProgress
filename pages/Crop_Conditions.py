03b329a02df76dc01e1ffb57577004664f0f1c1b
DanieleMarobin <daniele.marobin@hotmail.com>
2023-01-20 08:45:15 +0100

    Minor Changes

--- pages/Crop_Conditions.py
+++ pages/Crop_Conditions.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import func as rs
+import func as fu
 
 st.set_page_config(page_title='Crop Conditions',layout='wide',initial_sidebar_state='expanded')
 st.markdown("### Crop Conditions")
@@ -7,20 +7,20 @@ st.markdown("---")
 
 with st.sidebar:
     col11, col21 = st.columns(2)
-    add_grain = st.selectbox("Choose a Class", rs.grains_class,2)
+    add_grain = st.selectbox("Choose a Class", fu.grains_class,2)
     if add_grain == 'WINTER'.title():
-        add_region = st.selectbox("Choose a Region", rs.wwht_regions)
+        add_region = st.selectbox("Choose a Region", fu.wwht_regions)
     elif add_grain == 'SPRING, (EXCL DURUM)'.title():
-        add_region = st.selectbox("Choose a Region", rs.swht_regions)
+        add_region = st.selectbox("Choose a Region", fu.swht_regions)
     elif add_grain == 'SPRING, DURUM'.title():
-        add_region = st.selectbox("Choose a Region", rs.durum_regions)
+        add_region = st.selectbox("Choose a Region", fu.durum_regions)
     
     hovermode = st.selectbox('Hovermode',['x', 'y', 'closest', 'x unified', 'y unified'],index=2)
 
-df = rs.get_conditions(add_region, add_grain)
-st.plotly_chart(rs.get_conditions_chart(df, add_region, add_grain, hovermode=hovermode), use_container_width=True)
+df = fu.get_conditions(add_region, add_grain)
+st.plotly_chart(fu.get_conditions_chart(df, add_region, add_grain, hovermode=hovermode), use_container_width=True)
 
-fig_model = rs.get_yields_charts(df, add_region, add_grain, hovermode=hovermode)
+fig_model = fu.get_yields_charts(df, add_region, add_grain, hovermode=hovermode)
 
 for f in fig_model:
     st.markdown("---")
